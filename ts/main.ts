1dd5cbb54a24794973ef371cc9cba257f57bc2d0
Tregilish <joejr@j-d-c.biz>
2021-10-09 16:21:50 -0700

    Created function to test if text box has data

--- ts/main.ts
+++ ts/main.ts
@@ -5,5 +5,22 @@ window.onload = function() {
 }
 
 function main():void {
-    alert("Reg button was pushed");
+    isTextPresent("first-name","First name is required");
+    isTextPresent("last-name","Last name is required");
+}
+
+/**
+ * Returns true if the textBox with the given id has some text inside it.
+ * @param id The id of the <input type="text"> to validate
+ * @param errMsg The message to display in the sibling span of the textBox
+ */
+function isTextPresent(id:string, errMsg:string):boolean {
+    let txtBox = <HTMLInputElement>document.getElementById(id);
+    let txtBoxValue = txtBox.value;
+    if (txtBoxValue == "") {
+        let errSpan = <HTMLElement>txtBox.nextElementSibling;
+        errSpan.innerText = errMsg;
+        return false;
+    }
+    return true;
 }
