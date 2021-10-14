3f43c18ffe87027ca7564d4a7d926831b1510e5e
Tregilish <joejr@j-d-c.biz>
2021-10-13 18:43:40 -0700

    Added DOB validation.

--- ts/main.ts
+++ ts/main.ts
@@ -9,6 +9,22 @@ function main():void {
     resetErrorsMessage();
     isTextPresent("first-name","First name is required");
     isTextPresent("last-name","Last name is required");
+
+    // Validate Date
+    let dobBox = <HTMLInputElement>document.getElementById("dob");
+    let dob = dobBox.value;
+    if (!isValidDate(dob)) {
+        dobBox.nextElementSibling.innerHTML = "Invalid format.Format should be mm/dd/yyyy.";
+    }
+}
+
+function isValidDate(input:string):boolean{
+    // Validating  mm/dd/yyyy and m/d/yyyy 
+
+    let pattern = /^\d{1,2}\/\d{1,2}\/\d{4}$/g
+    let result = pattern.test(input);
+    return result;
+
 }
 
 /**
